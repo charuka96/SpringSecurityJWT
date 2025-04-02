e9d7ee4cf7d209f7056c2cf1f40ad08d345baffc
charuka96 <chaukathisaru96@gmail.com>
2025-04-02 20:43:42 +0530

    create passwordEncoder

--- src/main/java/com/SpringSecurity/JWTAuthentication/config/SecurityConfig.java
+++ src/main/java/com/SpringSecurity/JWTAuthentication/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.SpringSecurity.JWTAuthentication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,4 +24,19 @@ public class SecurityConfig {
               .httpBasic(Customizer.withDefaults())
               .build();
     }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder(12);
+    }
+
 }
+
+
